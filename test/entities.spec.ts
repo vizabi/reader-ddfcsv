b41fe18fab408d38692ea9be779a47f94589097d
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-10-18 15:45:50 +0300

    add extra tests for entities

--- test/entities.spec.ts
+++ test/entities.spec.ts
@@ -1,10 +1,13 @@
 import * as chai from 'chai';
+import { cloneDeep } from 'lodash';
+import { parallel } from 'async';
 import { isNumber } from 'lodash';
 import { BackendFileReader, Ddf } from '../src/index';
 
 const backendFileReader = new BackendFileReader();
 const GLOBALIS_PATH = './test/fixtures/systema_globalis';
 const GLOBALIS_TINY_PATH = './test/fixtures/systema_globalis_tiny';
+const PRESENTATION_SET = './test/fixtures/presentation_set';
 
 const expect = chai.expect;
 
@@ -248,6 +251,55 @@ describe('when entities checking', () => {
 
       expect(!!err).to.be.false;
       expect(data.length).to.equal(EXPECTED_RECORDS_COUNT);
+
+      done();
+    });
+  });
+
+  it('should filters work properly ', done => {
+    const EXPECTED_UNSTATE_QUANTITY = 195;
+    const EXPECTED_NON_UNSTATE_QUANTITY = 78;
+    const EXPECTED_UNSTATE_LESS_QUANTITY = 23;
+    const requestTemplate = {
+      language: 'en',
+      from: 'entities',
+      animatable: 'time',
+      select: {
+        key: [
+          'geo'
+        ],
+        value: [
+          'name',
+          'world_4region'
+        ]
+      },
+      where: {},
+      join: {},
+      order_by: [
+        'rank'
+      ]
+    };
+
+    const getExpectedAction = (whereClause) => onGotResult => {
+      const ddf = new Ddf(PRESENTATION_SET, backendFileReader);
+      const request = cloneDeep(requestTemplate);
+
+      request.where = whereClause;
+
+      ddf.ddfRequest(request, (err, data: any[]) => onGotResult(err, data));
+    };
+
+    parallel({
+      allEntities: getExpectedAction({}),
+      unStateEntities: getExpectedAction({$and: [{'un_state': true}]}),
+      nonUnStateEntities: getExpectedAction({$and: [{'un_state': false}]}),
+      unStateLessEntities: getExpectedAction({$and: [{'un_state': {$exists: false}}]})
+    }, (err, results) => {
+      expect(!!err).to.be.false;
+      expect((<any[]>results.nonUnStateEntities).length).to.equal(EXPECTED_NON_UNSTATE_QUANTITY);
+      expect((<any[]>results.unStateEntities).length).to.equal(EXPECTED_UNSTATE_QUANTITY);
+      expect((<any[]>results.unStateLessEntities).length).to.equal(EXPECTED_UNSTATE_LESS_QUANTITY);
+      expect((<any[]>results.allEntities).length).to.equal(EXPECTED_UNSTATE_QUANTITY + EXPECTED_NON_UNSTATE_QUANTITY + EXPECTED_UNSTATE_LESS_QUANTITY);
 
       done();
     });
