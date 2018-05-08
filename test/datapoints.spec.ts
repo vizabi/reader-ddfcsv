476c4ea16319479e9b18c27c74e5a19dc11b72e7
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-05-08 17:08:27 +0300

    feat(query optimization): optimize slow query based on $in clause
    Closes #77

--- test/datapoints.spec.ts
+++ test/datapoints.spec.ts
@@ -1,9 +1,12 @@
 import * as chai from 'chai';
-import { getDDFCsvReaderObject } from '../src/index';
+import {getDDFCsvReaderObject} from '../src/index';
 
 const expect = chai.expect;
 
 const GLOBALIS_PATH = './test/fixtures/systema_globalis';
+const BIG_PATH = './test/fixtures/ddf--gapminder--population.big';
+const POP_WPP_PATH = './test/fixtures/population_wpp';
+const STATIC_ASSETS = './test/fixtures/static-assets';
 
 describe('Datapoints supporting', () => {
   it('condition under join supporting', done => {
@@ -52,6 +55,191 @@ describe('Datapoints supporting', () => {
 
       expect(data.length).to.equal(1155);
       expect(countryAntData).to.be.an('array').that.is.empty;
+
+      done();
+    });
+  });
+
+  it('big 1', done => {
+    const reader = getDDFCsvReaderObject();
+
+    reader.init({path: BIG_PATH});
+    reader.read({
+      language: 'en',
+      from: 'datapoints',
+      animatable: 'year',
+      select: {
+        key: [
+          'geo',
+          'year',
+          'gender',
+          'age'
+        ],
+        value: [
+          'population'
+        ]
+      },
+      where: {
+        $and: [
+          {
+            geo: '$geo'
+          },
+          {
+            year: '$year'
+          },
+          {
+            gender: '$gender'
+          },
+          {
+            age: '$age'
+          }
+        ]
+      },
+      join: {
+        $geo: {
+          key: 'geo',
+          where: {
+            $and: [
+              {
+                $or: [
+                  {
+                    un_state: true
+                  },
+                  {
+                    'is--global': true
+                  },
+                  {
+                    'is--world_4region': true
+                  }
+                ]
+              },
+              {
+                geo: {
+                  $in: [
+                    'world'
+                  ]
+                }
+              }
+            ]
+          }
+        },
+        $year: {
+          key: 'year',
+          where: {
+            year: '2018'
+          }
+        },
+        $gender: {
+          key: 'gender',
+          where: {
+            gender: {
+              $in: [
+                'female'
+              ]
+            }
+          }
+        },
+        $age: {
+          key: 'age',
+          where: {
+            age: {
+              $nin: [
+                '80plus',
+                '100plus'
+              ]
+            }
+          }
+        }
+      },
+      order_by: [
+        'year'
+      ]
+    }).then(result => {
+      const expectedResult = require('./result-fixtures/in-clause-under-conjunction-1.json');
+
+      expect(result).to.deep.equal(expectedResult);
+
+      done();
+    });
+  });
+
+  it('big 2', done => {
+    const reader = getDDFCsvReaderObject();
+
+    reader.init({path: POP_WPP_PATH});
+    reader.read({
+      language: 'en',
+      from: 'datapoints',
+      animatable: 'year',
+      select: {
+        key: [
+          'country_code',
+          'year',
+          'gender',
+          'age'
+        ],
+        value: [
+          'population'
+        ]
+      },
+      where: {
+        $and: [
+          {
+            country_code: '$country_code'
+          }
+        ]
+      },
+      join: {
+        $country_code: {
+          key: 'country_code',
+          where: {
+            country_code: {
+              $in: [
+                '900'
+              ]
+            }
+          }
+        }
+      },
+      order_by: [
+        'country_code',
+        'year',
+        'gender',
+        'age'
+      ]
+    }).then(result => {
+      const expectedResult = require('./result-fixtures/in-clause-under-conjunction-2.json');
+
+      expect(result).to.deep.equal(expectedResult);
+
+      done();
+    });
+  });
+
+  it('assets', done => {
+    const reader = getDDFCsvReaderObject();
+
+    reader.init({path: STATIC_ASSETS});
+    reader.read({
+      language: 'en',
+      from: 'datapoints',
+      animatable: 'time',
+      select: {
+        key: ['geo', 'time'],
+        value: ['income_mountains']
+      },
+      where: {
+        $and: [{geo: '$geo'}, {time: '$time'}]
+      },
+      join: {
+        $geo: {key: 'geo', where: {geo: {$in: ['world']}}},
+        $time: {key: 'time', where: {time: '2015'}}
+      },
+      order_by: ['geo', 'time']
+    }).then(result => {
+      const expectedResult = require('./result-fixtures/datapoints-assets.json');
+
+      expect(result).to.deep.equal(expectedResult);
 
       done();
     });
