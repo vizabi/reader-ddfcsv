5d484205d01425561f33ae699a03ea848768c403
Oleksandra korel Kalinina <korery@gmail.com>
2018-06-08 12:00:22 +0300

    chore(*): add public properties to DdfCsvError

--- src/ddfcsv-error.ts
+++ src/ddfcsv-error.ts
@@ -3,8 +3,15 @@ export const JSON_PARSING_ERROR = 'JSON parsing error';
 export const CSV_PARSING_ERROR = 'CSV parsing error';
 export const DDF_ERROR = 'DDF error';
 
+/* tslint: disable-next-line */
 export class DdfCsvError extends Error {
-  constructor(message: string, public details, public file?: string) {
+  name: string;
+  message: string;
+  public details: any;
+  public file: string | null;
+  stack: string;
+
+  constructor(message: string, details, file?: string) {
     super();
     this.name = 'DdfCsvError';
     this.message = `${message} [filepath: ${file}]. ${details}.`;
