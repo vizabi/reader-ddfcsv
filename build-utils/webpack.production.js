a9aff557e9a82c0c703afb3ea3a202d062d1f99f
buchslava <vyacheslav.chub@gmail.com>
2018-08-23 17:04:23 +0300

    add test cases to dist

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
 module.exports = (env) => ({
-    entry: './src/index.ts',
+    entry: {
+        'vizabi-ddfcsv-reader-node': './src/index.ts',
+        'test-cases-concepts': './test/definition/test-cases/concepts.ts',
+        'test-cases-entities': './test/definition/test-cases/entities.ts'
+    },
     output: {
         path: path.join(__dirname, '../dist'),
-        filename: 'vizabi-ddfcsv-reader-node.js',
+        filename: '[name].js',
         libraryTarget: 'commonjs'
     }
 });
---
+++ dist/lib/src/file-readers/github-file-reader.d.ts
@@ -0,0 +1,6 @@
+import { IReader } from '../interfaces';
+export declare class GithubFileReader implements IReader {
+    recordTransformer: Function;
+    setRecordTransformer(recordTransformer: any): void;
+    readText(filePath: any, onFileRead: any): void;
+}
