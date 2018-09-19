7cf2eb9672fef2a7f157973ba58fa9d9e081f9bb
Oleksandra korel Kalinina <korery@gmail.com>
2018-09-19 12:45:31 +0300

    chore(*): refactor to work with WS

--- test/definition/schema-definition.spec.ts
+++ test/definition/schema-definition.spec.ts
@@ -3,7 +3,7 @@ import { getDDFCsvReaderObject } from '../../src/index';
 import {
   BASE_PATH,
   EXPECTS_EXACTLY_ONE_ERROR,
-  getAmountOfErrors,
+  getAmountOfErrors, GLOBALIS_PATH,
   notExpectedError,
   selectKeyClauseContainsUnavailableItems,
   selectValueClauseContainsUnavailableItems2,
@@ -14,6 +14,88 @@ import { CONCEPTS, DATAPOINTS, ENTITIES } from 'ddf-query-validator';
 const expect = chai.expect;
 
 describe('Schemas definition errors in query', () => {
+  describe('should never happen for happy flow', () => {
+    it(`when requests \'concepts.schema\' in \'${BASE_PATH + GLOBALIS_PATH}\' dataset with no \'select.value\'`, async () => {
+      const reader = getDDFCsvReaderObject();
+
+      reader.init({ path: BASE_PATH });
+
+      const query = {
+        select: {
+          key: [ 'key', 'value' ]
+        },
+        from: 'concepts.schema',
+        order_by: [ 'key', { value: 1 } ]
+      };
+      const result = await reader.read(query);
+
+      expect(result.length).to.equal(15);
+    });
+
+    it(`when requests \'concepts.schema\' in \'${BASE_PATH + GLOBALIS_PATH}\' dataset with empty \'select.value\'`, async () => {
+      const reader = getDDFCsvReaderObject();
+
+      reader.init({ path: BASE_PATH });
+
+      const query = {
+        select: {
+          key: [ 'key', 'value' ],
+          value: []
+        },
+        from: 'concepts.schema'
+      };
+      const result = await reader.read(query);
+      expect(result.length).to.equal(15);
+    });
+
+    it(`when requests \'entities.schema\' in \'${BASE_PATH + GLOBALIS_PATH}\' dataset with \'select.value\'`, async () => {
+      const reader = getDDFCsvReaderObject();
+
+      reader.init({ path: BASE_PATH });
+
+      const query = {
+        select: {
+          key: [ 'key', 'value' ],
+          value: [ 'value' ]
+        },
+        from: 'entities.schema'
+      };
+      const result = await reader.read(query);
+      expect(result.length).to.equal(118);
+    });
+
+    it(`when requests \'datapoints.schema\' in \'${BASE_PATH + GLOBALIS_PATH}\' dataset with \'select.value\'`, async () => {
+      const reader = getDDFCsvReaderObject();
+
+      reader.init({ path: BASE_PATH });
+
+      const query = {
+        select: {
+          key: [ 'key', 'value' ],
+          value: [ 'value' ]
+        },
+        from: 'datapoints.schema'
+      };
+      const result = await reader.read(query);
+      expect(result.length).to.equal(1076);
+    });
+
+    it(`when requests \'*.schema\' in \'${BASE_PATH + GLOBALIS_PATH}\' dataset with \'select.value\'`, async () => {
+      const reader = getDDFCsvReaderObject();
+
+      reader.init({ path: BASE_PATH });
+
+      const query = {
+        select: {
+          key: [ 'key', 'value' ],
+          value: [ 'value' ]
+        },
+        from: '*.schema'
+      };
+      const result = await reader.read(query);
+      expect(result.length).to.equal(1209);
+    });
+  });
 
   describe('should be produced only for \'select\' section', () => {
     [ DATAPOINTS, ENTITIES, CONCEPTS ].forEach((queryType: string) => {
--- test/structure/query-operators-structure.spec.ts
+++
@@ -1,8 +0,0 @@
-import * as chai from 'chai';
-const expect = chai.expect;
-
-xdescribe('Query operators structure errors in query', () => {
-  it('should be processed correctly', () => {
-    expect(true).to.be.ok;
-  });
-});
