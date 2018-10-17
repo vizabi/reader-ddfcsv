df3eac361be5f3ed1470c204af67ea3c7b756414
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-10-17 10:24:21 +0300

    webpack should process query validator

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = () => ({
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules\/(?![ddf\-query\-validator])/,
+                exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
